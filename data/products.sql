fc47e72cb999f589240ec4cb8005856d64e54a68
LoickDev97438 <loickcherimont@gmail.com>
2022-06-27 23:18:08 +0400

    Setup e_shop database by creating products table

---
+++ data/products.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(128) NOT NULL,
+    img VARBINARY(128) NOT NULL,
+    price DECIMAL(5,2) NOT NULL,
+    quantity INT,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO products
+    (title, img, price, quantity)
+VALUES
+    ("Laptop", "laptop.jpg", 500.99, 5),
+    ("Smartphone", "smartphone.jpg", 300.50, 10),
+    ("Tablet", "tablet.jpg", 100.30, 3);
